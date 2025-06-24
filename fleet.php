371409a9479493f242ad5ecef31da408096203cb
mtahabinrashid <muhammadtahabinrashid@gmail.com>
2025-06-25 03:02:24 +0500

    inner page

---
+++ fleet.php
@@ -0,0 +1,165 @@
+<?php include 'include/header.php' ?>
+
+<section class="inner_banner">
+    <figure class="inner_banner_img">
+        <img src="images/inner-bg.webp" alt="image" class="img-fluid">
+    </figure>
+    <div class="container">
+        <div class="inner_banner_content">
+            <h1>EXCLUSIVE PRIVATE JETS</h1>
+        </div>
+    </div>
+</section>
+
+<section class="fleet_section padd-y">
+    <div class="container">
+        <div class="fleet_tab_nav">
+            <ul class="nav nav-pills" id="pills-tab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="pills-one-tab" data-toggle="pill" href="#pills-one" role="tab"
+                        aria-controls="pills-one" aria-selected="true">GULFSTREAM G650</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="pills-two-tab" data-toggle="pill" href="#pills-two" role="tab"
+                        aria-controls="pills-two" aria-selected="false">GULFSTREAM G450</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="pills-three-tab" data-toggle="pill" href="#pills-three" role="tab"
+                        aria-controls="pills-three" aria-selected="false">PRAETOR 600</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="pills-four-tab" data-toggle="pill" href="#pills-four" role="tab"
+                        aria-controls="pills-four" aria-selected="false">CHALLENGER 350 / 3500</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="pills-five-tab" data-toggle="pill" href="#pills-five" role="tab"
+                        aria-controls="pills-five" aria-selected="false">PRAETOR 500</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="pills-six-tab" data-toggle="pill" href="#pills-six" role="tab"
+                        aria-controls="pills-six" aria-selected="false">PHENOM 300</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="pills-seven-tab" data-toggle="pill" href="#pills-seven" role="tab"
+                        aria-controls="pills-seven" aria-selected="false">SIKORSKY S-76</a>
+                </li>
+            </ul>
+        </div>
+        <div class="tab-content fleet_tab_content mt-5" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-one" role="tabpanel" aria-labelledby="pills-one-tab">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-md-5">
+                        <figure class="fleet_img">
+                            <img src="images/fleet-img1.webp" alt="image" class="img-fluid">
+                        </figure>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="fleet_content text-center">
+                            <h3>GULFSTREAM G650</h3>
+                            <h4>A TITAN OF INDUSTRY</h4>
+                            <a href="" class="themeBtn mx-auto">ACCESS ULTRA-LONG-RANGE</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="pills-two" role="tabpanel" aria-labelledby="pills-two-tab">
+                <div class="row justify-content-center align-items-center flex-row-reverse">
+                    <div class="col-md-5">
+                        <figure class="fleet_img">
+                            <img src="images/fleet-img2.webp" alt="image" class="img-fluid">
+                        </figure>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="fleet_content text-center">
+                            <h3>GULFSTREAM G450</h3>
+                            <h4>ENHANCING ENTERPRISING OPPORTUNITIES</h4>
+                            <a href="" class="themeBtn mx-auto">ACCESS LARGE CABIN</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="pills-three" role="tabpanel" aria-labelledby="pills-three-tab">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-md-5">
+                        <figure class="fleet_img">
+                            <img src="images/fleet-img3.webp" alt="image" class="img-fluid">
+                        </figure>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="fleet_content text-center">
+                            <h3>PRAETOR 600</h3>
+                            <h4>AN ASCENDING PATHFINDER PEDIGREE</h4>
+                            <a href="" class="themeBtn mx-auto">ACCESS SUPER-MIDSIZE</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="pills-four" role="tabpanel" aria-labelledby="pills-four-tab">
+                <div class="row justify-content-center align-items-center flex-row-reverse">
+                    <div class="col-md-5">
+                        <figure class="fleet_img">
+                            <img src="images/fleet-img4.webp" alt="image" class="img-fluid">
+                        </figure>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="fleet_content text-center">
+                            <h3>CHALLENGER 350 / 3500</h3>
+                            <h4>TRUE EXECUTIVE EVOLUTION</h4>
+                            <a href="" class="themeBtn mx-auto">ACCESS SUPER-MIDSIZE</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="pills-five" role="tabpanel" aria-labelledby="pills-fivee-tab">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-md-5">
+                        <figure class="fleet_img">
+                            <img src="images/fleet-img5.webp" alt="image" class="img-fluid">
+                        </figure>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="fleet_content text-center">
+                            <h3>PRAETOR 500</h3>
+                            <h4>AN EXPLORER OF ENDLESS HORIZONS</h4>
+                            <a href="" class="themeBtn mx-auto">ACCESS MIDSIZE</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="pills-six" role="tabpanel" aria-labelledby="pills-sixe-tab">
+                <div class="row justify-content-center align-items-center flex-row-reverse">
+                    <div class="col-md-5">
+                        <figure class="fleet_img">
+                            <img src="images/fleet-img6.webp" alt="image" class="img-fluid">
+                        </figure>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="fleet_content text-center">
+                            <h3>PHENOM 300</h3>
+                            <h4>PERFECTLY ADAPTED FOR ADVENTURE</h4>
+                            <a href="" class="themeBtn mx-auto">ACCESS LIGHT CABIN</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="pills-seven" role="tabpanel" aria-labelledby="pills-sevenee-tab">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-md-5">
+                        <figure class="fleet_img">
+                            <img src="images/fleet-img7.webp" alt="image" class="img-fluid">
+                        </figure>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="fleet_content text-center">
+                            <h3>SIKORSKY S-76</h3>
+                            <h4>TREND SETTING PERFORMANCE & PRECISION</h4>
+                            <a href="" class="themeBtn mx-auto">ACCESS HELICOPTER</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'include/footer.php' ?>
---
+++ programs.php
@@ -0,0 +1,205 @@
+<?php include 'include/header.php' ?>
+
+<section class="inner_banner">
+    <figure class="inner_banner_img">
+        <img src="images/inner-bg.webp" alt="image" class="img-fluid">
+    </figure>
+    <div class="container">
+        <div class="inner_banner_content">
+            <h1>PRIVATE JET PROGRAMS</h1>
+
+        </div>
+    </div>
+</section>
+
+<section class="programs_section padd-y">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <figure class="programs_img">
+                    <img src="images/fleet-img1.webp" alt="image" class="img-fluid">
+                </figure>
+            </div>
+            <div class="col-md-6">
+                <div class="programs_content">
+                    <h3>fractional jet ownership</h3>
+                    <p>
+                        The pinnacle of the Flexjet experience. Secure the main benefits of whole aircraft ownership
+                        without a single one of the burdens. Unlock consistency, exceptional service and guaranteed
+                        access for a predictable cost and approachable obligation.
+                    </p>
+                    <ul class="programs_list">
+                        <li>
+                            <div class="programs_list_text">
+                                <h4>ideal for those who fly</h4>
+                                <h6>50+ HOURS ANNUALLY</h6>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="programs_list_text">
+                                <h4>COST COMPONENTS</h4>
+                                <p>Upfront Cost (Asset Purchase)</p>
+                                <p>Monthly Management Fee</p>
+                                <p>Hourly Occupied Rate</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="programs_list_text">
+                                <h4>PROGRAM FEATURES</h4>
+                                <p>Guaranteed Access</p>
+                                <p>Short Callout Time</p>
+                                <p>Interchange Opportunities</p>
+                            </div>
+                        </li>
+                    </ul>
+                    <a href="" class="themeBtn">LEARN MORE</a>
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center flex-row-reverse">
+            <div class="col-md-6">
+                <figure class="programs_img">
+                    <img src="images/fleet-img1.webp" alt="image" class="img-fluid">
+                </figure>
+            </div>
+            <div class="col-md-6">
+                <div class="programs_content">
+                    <h3>LEASE</h3>
+                    <p>
+                        The flexibility of liquidity. Whether traveling for business or pleasure, aircraft leasing
+                        offers the advantages of purchasing a fractional jet share, replacing the upfront capital outlay
+                        to purchase a share of an actual aircraft with a structured monthly installment.
+                    </p>
+                    <ul class="programs_list">
+                        <li>
+                            <div class="programs_list_text">
+                                <h4>ideal for those who fly</h4>
+                                <h6>50+ HOURS ANNUALLY</h6>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="programs_list_text">
+                                <h4>COST COMPONENTS</h4>
+                                <p>Lease Deposit</p>
+                                <p>Monthly Lease Payment</p>
+                                <p>Monthly Management Fee</p>
+                                <p>Hourly Occupied Rate</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="programs_list_text">
+                                <h4>PROGRAM FEATURES</h4>
+                                <p>Guaranteed Access</p>
+                                <p>Short Callout Time</p>
+                                <p>Interchange Opportunities</p>
+                            </div>
+                        </li>
+                    </ul>
+                    <a href="" class="themeBtn">LEARN MORE</a>
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <figure class="programs_img">
+                    <img src="images/fleet-img1.webp" alt="image" class="img-fluid">
+                </figure>
+            </div>
+            <div class="col-md-6">
+                <div class="programs_content">
+                    <h3>jet card</h3>
+                    <p>
+                        An enticing way to experience the Flexjet difference first-hand. This convenient and exploratory
+                        debit-style model is meant for new entrants to the private aviation space.
+                    </p>
+                    <ul class="programs_list">
+                        <li>
+                            <div class="programs_list_text">
+                                <h4>ideal for those who fly</h4>
+                                <h6>25+ HOURS ANNUALLY</h6>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="programs_list_text">
+                                <h4>COST COMPONENTS</h4>
+                                <p>Initial Deposit</p>
+                                <p>No Asset Purchase</p>
+                                <p>No Monthly Management Fee</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="programs_list_text">
+                                <h4>PROGRAM FEATURES</h4>
+                                <p>Immediate Private Jet Access</p>
+                                <p>Inclusive Pricing</p>
+                                <p>Advanced Scheduling</p>
+                            </div>
+                        </li>
+                    </ul>
+                    <a href="" class="themeBtn">LEARN MORE</a>
+                </div>
+            </div>
+        </div>
+        <div class="title pb-5">
+            <h2 class="heading text-center">FLEXJET EXCLUSIVE PROGRAM ENHANCEMENTS</h2>
+        </div>
+        <div class="row align-items-center flex-row-reverse">
+            <div class="col-md-6">
+                <figure class="programs_img">
+                    <img src="images/fleet-img1.webp" alt="image" class="img-fluid">
+                </figure>
+            </div>
+            <div class="col-md-6">
+                <div class="programs_content">
+                    <h3>unlimited access</h3>
+                    <p>
+                        As the exclusive fractional home of the Gulfstream G650, only Flexjet offers this innovative way
+                        to utilize one of the industry’s most capable private jets. With a single fractional or lease
+                        share, this program grants you unrestricted use of our entire fleet at attractive interchange
+                        rates.
+                    </p>
+                    <ul class="programs_list">
+                        <li>
+                            <div class="programs_list_text">
+                                <h4>FEATURED ADVANTAGES</h4>
+                                <p>Right-size your aircraft and maximize the efficiency of each trip</p>
+                                <p>Fly friends, family members and associates on any jet in our fleet</p>
+                                <p>Overall, the flexibility to unlock the entire Flexjet fleet</p>
+                            </div>
+                        </li>
+                    </ul>
+                    <a href="" class="themeBtn">LEARN MORE</a>
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <figure class="programs_img">
+                    <img src="images/fleet-img1.webp" alt="image" class="img-fluid">
+                </figure>
+            </div>
+            <div class="col-md-6">
+                <div class="programs_content">
+                    <h3>PRIVATE HELICOPTERS</h3>
+                    <p>
+                        The cure for the common commute. Flexjet helicopters are the epitome of airborne excellence and
+                        VIP travel, simultaneously offering unparalleled reliability, luxury and superior performance.
+                        Our helicopters come with the same personalized, concierge-level service as our private jets.
+                    </p>
+                    <ul class="programs_list">
+                        <li>
+                            <div class="programs_list_text">
+                                <h4>AVAILABLE ACCESS OPTIONS</h4>
+                                <p>Helicopter Lease</p>
+                                <p>Helicopter Card</p>
+                                <p>Selective Charter</p>
+                            </div>
+                        </li>                        
+                    </ul>
+                    <a href="" class="themeBtn">LEARN MORE</a>
+                </div>
+            </div>
+        </div>
+</section>
+
+<?php include 'include/footer.php' ?>
